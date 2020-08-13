8e0cbb2826535fd88f0d2fbb9202171ed28860c9
heshixiang <heshixiang@bytedance.com>
2020-08-13 15:30:34 +0800

    change demo2

--- demo2.java
+++ demo2.java
@@ -1,5 +1,8 @@
 public class demo2 {
     public static void main(String[] args){
+
         System.out.println("hello world demo2");
+        System.out.println("hello world xxxxx");
+        System.out.println("hello world kkkkk");
     }
 }
